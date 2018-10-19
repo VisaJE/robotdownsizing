5c362d40060cf29987dd0bcd4045bec357e929d9
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-10-19 21:04:39 +0300

    Asdsa

--- linefollower.py
+++ linefollower.py
@@ -15,8 +15,11 @@ while(True):
 
     if(on_dark):
         left_motor.run_timed(time_sp=200, speed_sp=900, stop_action='brake')
+        print("vasen")
     if(on_light):
         right_motor.run_timed(time_sp=200, speed_sp=900, stop_action='brake')
+        print("oikea")
     left_motor.wait_while('running')
     right_motor.wait_while('running')
+    print("kierros")
     sleep(0.2)
