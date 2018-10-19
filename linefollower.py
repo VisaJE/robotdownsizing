1a6911f5af75f70493aa111ca18ae3c0cb620444
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-10-19 20:57:11 +0300

    ads|

--- linefollower.py
+++ linefollower.py
@@ -1,15 +1,15 @@
 from ev3dev.ev3 import *
 
-cl=ColorSensor('Inp1')
-on_dark=true
-on_light=true
+cl=ColorSensor()
+on_dark=True
+on_light=True
 
 cl.mode='COL-REFLECT'
-while(true):
+while(True):
     left_motor=LargeMotor('OutB')
     right_motor=LargeMotor('OutA')
-    on_light=cl.value>40
-    on_dark=cl.value<60
+    on_light=cl.value()>40
+    on_dark=cl.value()<60
 
     if(on_dark):
         left_motor.run_timed(time_sp=200, speed_sp=900, stop_action='brake')
