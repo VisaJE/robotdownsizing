59bcf2968aca2b3c852b24bea3899060cc94ff78
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-10-19 20:37:42 +0300

    ;ol

---
+++ linefollower.py
@@ -0,0 +1,13 @@
+from ev3dev.ev3 import *
+from time   import sleep
+m = LargeMotor('outB')
+
+m.run_forever(speed_sp=200)   # equivalent to power=20 in EV3-G
+sleep(5)
+m.stop(stop_action="coast")
+sleep(4)
+
+m.run_forever()
+sleep(5)
+m.stop()
+sleep(4)
