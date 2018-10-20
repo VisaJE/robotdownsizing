bd2d00fe293e40f58d793b89a3fc6f86ea1ad009
VisaJE <eemil@live.com>
2018-10-20 22:19:15 +0300

    Automated message, ignore

--- testi.py
+++ testi.py
@@ -1,14 +1,24 @@
 from ev3dev.ev3 import *
 from time import sleep
 from ColorStuff import ColorStuff
-
+from turn_robot import *
 
 cs = ColorStuff()
 us = UltrasonicSensor() 
+lm = LargeMotor('outB')
+rm = LargeMotor('outA')
+touch_sensor_right=TouchSensor('in2')
+touch_sensor_left=TouchSensor('in3')
 
 us.mode='US-DIST-CM'
 state = 0
-while True:
-	print("statte " + str(state))
-	print(cs.getColorFUCK()[0])
+turn_right(lm, rm, 45)
+sleep(5)
+turn_left(lm, rm, 45)
+sleep(5)
+turn_right(lm, rm, 90)
+sleep(5)
+turn_left(lm, rm, 180)
+sleep(5)
+move(lm, rm, touch_sensor_left, touch_sensor_right, distance=10)
 
