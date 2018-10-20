9afe26dc2f1939db9620a1449480b810696692d8
coldDitch <juuso.kylmaoja@aalto.fi>
2018-10-20 11:33:21 +0300

    pls

--- turn_robot.py
+++ turn_robot.py
@@ -1,11 +1,11 @@
 def turn(left_motor,right_motor,degrees=90):
     c=10
     if(degrees>0):
-        left_motor.run_timed(speed_sp=-900,time_sp=c*degrees,stop_action='brake')
-        right_motor.run_timed(speed_sp=900,time_sp=c*degrees,stop_action='brake')
+        left_motor.run_timed(speed_sp=-200,time_sp=c*degrees,stop_action='brake')
+        right_motor.run_timed(speed_sp=200,time_sp=c*degrees,stop_action='brake')
     else:
-        left_motor.run_timed(speed_sp=900,time_sp=c*degrees,stop_action='brake')
-        right_motor.run_timed(speed_sp=-900,time_sp=c*degrees,stop_action='brake')
+        left_motor.run_timed(speed_sp=200,time_sp=c*degrees,stop_action='brake')
+        right_motor.run_timed(speed_sp=-200,time_sp=c*degrees,stop_action='brake')
 
 left_motor=LargeMotor('outB')
 right_motor=LargeMotor('outA')
