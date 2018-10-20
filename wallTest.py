ae268ef67e7477935400680b64de78887b58e732
VisaJE <eemil@live.com>
2018-10-20 23:13:15 +0300

    Automated message, ignore

--- wallTest.py
+++ wallTest.py
@@ -2,6 +2,7 @@ from ev3dev.ev3 import *
 from time import sleep
 from ColorStuff import ColorStuff
 from turn_robot import *
+from follow_right_wall import FollowRight
 
 us = UltrasonicSensor() 
 lm = LargeMotor('outB')
