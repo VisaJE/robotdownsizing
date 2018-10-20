fd5837a3b2080481224650636e43f94c3dc79129
coldDitch <juuso.kylmaoja@aalto.fi>
2018-10-20 22:00:03 +0300

    Automated message, ignore

--- pylvas.py
+++ pylvas.py
@@ -21,19 +21,26 @@ class Pylvas_solver:
         while self.not_done:
             while self.step_forward():
                 pass
-            self.step_backwards()
+            if calculate_distance > 10:
+                self.fix_orientation()
+            else:
+                self.fix_position()
         return True
 
     def step_forward(self):
         orientation_change=self.find_orientation()
-        self.orientation=orientation_change
+        self.orientation-=orientation_change
         print("Orientation change: ", orientation_change)
-        if orientation_change>0 :
+        if orientation_change<0 :
             turn_left(self.left_motor,self.right_motor,orientation_change)
         else:
             turn_right(self.left_motor,self.right_motor,orientation_change)
         desired_dist=self.calculate_distance()
-        self.last_move,bump_l,bump_r=move(self.left_motor,self.right_motor, self.touch_sensor_left, self.touch_sensor_right,desired_dist)
+        self.last_moves,bump_l,bump_r=move(self.left_motor,self.right_motor, self.touch_sensor_left, self.touch_sensor_right,desired_dist)
+
+        self.position[0]+=math.cos(self.orientation/360*2*math.pi)*self.last_moves
+        self.position[1]+=math.sin(self.orientation/360*2*math.pi)*self.last_moves
+
         if bump_l:
             self.bumped_left=True
             #print("bumped left")
@@ -42,12 +49,14 @@ class Pylvas_solver:
             self.bumped_right=True
             #print("bumped right")
             return False
-        self.position=self.preferred[self.stage]
+
+        #self.position=self.preferred[self.stage]
         self.stage+=1
         return True
 
-    def step_backwards(self):
-        move_backwards(self.left_motor,self.right_motor, distance = self.last_move)
+    def fix_orientation(self):
+        move_backwards(self.left_motor,self.right_motor, distance = 10)
+
         degree = 5
         if self.bumped_right:
             print("turning left")
@@ -57,6 +66,15 @@ class Pylvas_solver:
             turn_right(self.left_motor,self.right_motor,degree)
         self.bumped_left=False
         self.bumped_right=False
+        self.position[0]+=math.cos(self.orientation/360*2*math.pi)*self.last_moves
+        self.position[1]+=math.sin(self.orientation/360*2*math.pi)*self.last_moves
+        return
+
+    def fix_position(self):
+        self.bumped_left=False
+        self.bumped_right=False
+        self.stage += 1
+        return
 
     def find_orientation(self):
         if self.position[0]-self.preferred[self.stage][0]==0:
