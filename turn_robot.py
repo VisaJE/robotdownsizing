7b23462e253052e768927e0c054b5de69da2f11a
coldDitch <juuso.kylmaoja@aalto.fi>
2018-10-20 20:06:12 +0300

    Automated message, ignore

--- turn_robot.py
+++ turn_robot.py
@@ -3,7 +3,7 @@ from time import sleep
 import math
 
 def turn_left(left_motor,right_motor,degrees=90, flag=1): # flag=1 turns left, -1 turns right
-    time = 60+math.abs(degrees)*540/45
+    time = 60+abs(degrees)*540/45
     left_motor.run_timed(speed_sp=flag*-200,time_sp=time,stop_action='brake')
     right_motor.run_timed(speed_sp=flag*200,time_sp=time,stop_action='brake')
     sleep(time/1000)
