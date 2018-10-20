f4ced82315f5e8494a09d208022b629b96dfed12
coldDitch <juuso.kylmaoja@aalto.fi>
2018-10-20 15:26:16 +0300

    asda

--- turn_robot.py
+++ turn_robot.py
@@ -22,15 +22,15 @@ def move(left_motor, right_motor, touch_sensor_left, touch_sensor_right, distanc
     mov = 0
     bumped_right=False
     bumped_left=False
-    epsilon = 0.1
-    while (touch_sensor_left.is_pressed or touch_sensor_right.is_pressed) and mov < distance:
+    epsilon = 2
+    while not (touch_sensor_left.is_pressed or touch_sensor_right.is_pressed) and mov < distance:
         if mov + epsilon < distance:
-            left_motor.run_timed(speed_sp=speed, time_sp=c*epsilon, stop_action='brake')
-            right_motor.run_timed(speed_sp=speed, time_sp=c*epsilon, stop_action='brake')
+            left_motor.run_timed(speed_sp=speed, time_sp=c*epsilon)
+            right_motor.run_timed(speed_sp=speed, time_sp=c*epsilon)
             mov += epsilon
         else:
-            left_motor.run_timed(speed_sp=speed, time_sp=c*(distance-mov), stop_action='brake')
-            right_motor.run_timed(speed_sp=speed, time_sp=c*(distance-mov), stop_action='brake')
+            left_motor.run_timed(speed_sp=speed, time_sp=c*(distance-mov))
+            right_motor.run_timed(speed_sp=speed, time_sp=c*(distance-mov))
             mov = distance
     bumped_right=touch_sensor_right.is_pressed
     bumped_left=touch_sensor_left.is_pressed
