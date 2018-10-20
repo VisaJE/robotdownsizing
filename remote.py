3fe331018844bb10ddee448908ca4396263b639c
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-10-20 18:25:22 +0300

    dd

--- remote.py
+++ remote.py
@@ -7,19 +7,20 @@ from math import *
 class Remote:
     def __init__(self):
         lm = LargeMotor('outB')
-		rm = LargeMotor('outA')
-	def inputLoop(self):
-		while True:
-			inp = input("")
-			if inp == 'q':
-				break
-			if inp == 'a':
-				lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
-			elif inp == 's':
-				lm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
-				rm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
-			elif inp == 'd':
-				rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
-			elif inp == 'w':
-				lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
-				rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+        rm = LargeMotor('outA')
+    def inputLoop(self):
+        while True:
+            inp = input("")
+            if inp == 'q':
+                break
+            if inp == 'a':
+                lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+            elif inp == 's':
+                lm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
+                rm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
+            elif inp == 'd':
+                rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+            elif inp == 'w':
+                lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+                rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+            sleep(0.25)
