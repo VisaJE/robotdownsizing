ac5ad50c0af7fb4448f0a9f503dfd99b6a4f9dbb
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-10-20 18:27:45 +0300

    dd

--- remote.py
+++ remote.py
@@ -9,18 +9,20 @@ class Remote:
         lm = LargeMotor('outB')
         rm = LargeMotor('outA')
     def inputLoop(self):
+    	print("remote start")
         while True:
             inp = input("")
             if inp == 'q':
                 break
             if inp == 'a':
-                lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+                self.lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
             elif inp == 's':
-                lm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
-                rm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
+                self.lm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
+                self.rm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
             elif inp == 'd':
-                rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+                self.rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
             elif inp == 'w':
-                lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
-                rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+                self.lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+                self.rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
             sleep(0.25)
+        print("remote exit")
