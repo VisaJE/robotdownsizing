00da7f47cc3c672d112bff9adacc4f8751f97836
VisaJE <eemil@live.com>
2018-10-20 22:24:26 +0300

    Automated message, ignore

--- turn_robot.py
+++ turn_robot.py
@@ -4,7 +4,7 @@ import math
 
 def turn_left(left_motor,right_motor,degrees=90, flag=1): # flag=1 turns left, -1 turns right
     print("Flag: ", flag)
-    time = 60+abs(degrees)*540/45
+    time = 60+abs(degrees)*530/45
     left_motor.run_timed(speed_sp=flag*200,time_sp=time,stop_action='brake')
     right_motor.run_timed(speed_sp=flag*-200,time_sp=time,stop_action='brake')
     sleep(time/1000)
@@ -43,7 +43,7 @@ def moveWithUV(left_motor, right_motor, touch_sensor_left, touch_sensor_right, u
     bumped_right=False
     bumped_left=False
     epsilon=1
-    pseudo_distance=1.24*distance
+    pseudo_distance=1.34*distance
     while (not (touch_sensor_left.is_pressed or touch_sensor_right.is_pressed) or (uv_sensor is not None and uv_sensor.value() > uvDist)) and mov < pseudo_distance:
         if mov + epsilon < pseudo_distance:
             left_motor.run_timed(speed_sp=-speed, time_sp=c*epsilon)
@@ -62,7 +62,7 @@ def move_backwards(left_motor, right_motor, distance=10,speed=500):
     c = 50
     mov = 0
     epsilon = 1
-    pseudo_distance = 1.24*distance
+    pseudo_distance = 1.34*distance
     while mov < pseudo_distance:
         if mov + epsilon < pseudo_distance:
             left_motor.run_timed(speed_sp=speed, time_sp=c*epsilon)
