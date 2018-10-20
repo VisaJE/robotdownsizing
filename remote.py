982cf2745fdf006b138f02b29d580a0fe68384bc
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-10-20 19:02:07 +0300

    dd

--- remote.py
+++ remote.py
@@ -3,6 +3,7 @@ from time   import sleep
 from collections import namedtuple
 from functools import reduce
 from math import *
+import curses
 
 class Remote:
     def __init__(self):
@@ -11,10 +12,18 @@ class Remote:
         self.mm = Motor('outC')
     def inputLoop(self):
         print("remote start")
-        while True:
-            inp = input("")
-            if inp == 'q':
-                break
+        screen = curses.initscr()
+        curses.cbreak()
+        screen.keypad(1)
+
+        screen.addstr("Enter q to quit ")
+        screen.refresh()
+        inp = ''
+
+        while inp != ord('q'):
+            inp = screen.getch()
+            screen.addch(inp)
+            screen.refresh()
             if inp == 'a':
                 self.rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
             elif inp == 's':
@@ -26,6 +35,9 @@ class Remote:
                 self.lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
                 self.rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
             elif inp == 'r':
-            	self.mm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+                self.mm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+            elif inp == 't':
+                self.mm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
             sleep(0.25)
+        curses.endwin()
         print("remote exit")
