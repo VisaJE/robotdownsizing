84659a5811eebf70a46d94e00da0ddc09e321edb
coldDitch <juuso.kylmaoja@aalto.fi>
2018-10-20 15:57:06 +0300

    asda

--- turn_robot.py
+++ turn_robot.py
@@ -7,8 +7,8 @@ def turn_left(left_motor,right_motor,degrees=90):
     t=90*14-100
     s=45*14-50+20
 
-    left_motor.run_timed(speed_sp=200,time_sp=s,stop_action='brake')
-    right_motor.run_timed(speed_sp=-200,time_sp=s,stop_action='brake')
+    left_motor.run_timed(speed_sp=200,time_sp=60+degrees*540/45,stop_action='brake')
+    right_motor.run_timed(speed_sp=-200,time_sp=60+degrees*540/45,stop_action='brake')
 
 
 def turn_right(left_motor,right_motor,degrees=90):
@@ -18,7 +18,7 @@ def turn_right(left_motor,right_motor,degrees=90):
 
 
 def move(left_motor, right_motor, touch_sensor_left, touch_sensor_right, distance=10,speed=500):
-    c = 10
+    c = 50
     mov = 0
     bumped_right=False
     bumped_left=False
