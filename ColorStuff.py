01e5b090e1b2c3af692a32a9cedf6262c11bda52
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-10-20 15:33:21 +0300

    dd

--- ColorStuff.py
+++ ColorStuff.py
@@ -31,6 +31,7 @@ class ColorStuff:
 
     def getClosestKnown(self, color1):
         dists = zip(self.knownColors.items(), map((lambda x: self.getDistance(color1, x[1]) ), self.knownColors.items() ))
+        dists = list(dists)
         print("mita vi" + str(len(dists)))
         return min(dists, key=lambda t: t[1])
 
