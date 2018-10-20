a0ad09b4dec779059210cf97ad071def4800867a
Arttu Tolvanen <arttu.tolvanen@aalto.fi>
2018-10-20 11:20:12 +0300

    movetus

--- turn_robot.py
+++ turn_robot.py
@@ -10,3 +10,22 @@ def turn(left_motor,right_motor,degrees=90):
 left_motor=LargeMotor('outB')
 right_motor=LargeMotor('outA')
 turn(left_motor,right_motor)
+
+
+
+def move(left_motor, right_motor, sensor, distance=10):
+    c = 10
+    mov = 0
+    while (sensor? and mov < distance):
+        if (mov + epsilon < distance):
+            left_motor.run_timed(speed_sp=500, time_sp=c*epsilon, stop_action='brake')
+            right_motor.run_timed(speed_sp=500, time_sp=c*epsilon, stop_action='brake')
+            mov += epsilon
+        else:
+            left_motor.run_timed(speed_sp=500, time_sp=c*(distance-mov), stop_action='brake')
+            right_motor.run_timed(speed_sp=500, time_sp=c*(distance-mov), stop_action='brake')
+            mov = distance
+        
+    return mov
+        
+        
