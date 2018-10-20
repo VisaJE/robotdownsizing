c2798c201a080c5b72b3f6897e62c493d78be24d
coldDitch <juuso.kylmaoja@aalto.fi>
2018-10-21 02:03:14 +0300

    Automated message, ignore

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ lm = LargeMotor('outB')
 rm = LargeMotor('outA')
 touch_sensor_right=TouchSensor('in2')
 touch_sensor_left=TouchSensor('in3')
-position=[75,-10]
+position=[75,0]
 preferred=[[75,20],[130,30], [75,73], [135,95], [130,135], [75,140], [75, 170]]
 solver=Pylvas_solver(lm,rm,touch_sensor_right,touch_sensor_left,position,preferred)
 solver.execute()
