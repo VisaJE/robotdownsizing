e3e7294a9e3492f77ef9eee6b2300d0e3615ac48
VisaJE <eemil@live.com>
2018-10-20 21:50:10 +0300

    Automated message, ignore

--- turn_robot.py
+++ turn_robot.py
@@ -15,9 +15,29 @@ def turn_right(left_motor,right_motor,degrees=90):
     return
 
 def move(left_motor, right_motor, touch_sensor_left, touch_sensor_right, distance=10,speed=500):
-    move(left_motor, right_motor, touch_sensor_left, touch_sensor_right, distance=10, speed=500, None, 0)
+    c = 50
+    mov = 0
+    bumped_right=False
+    bumped_left=False
+    epsilon=1
+    pseudo_distance=1.24*distance
+    while (not (touch_sensor_left.is_pressed or touch_sensor_right.is_pressed)) and mov < pseudo_distance:
+        if mov + epsilon < pseudo_distance:
+            left_motor.run_timed(speed_sp=-speed, time_sp=c*epsilon)
+            right_motor.run_timed(speed_sp=-speed, time_sp=c*epsilon)
+            mov += epsilon
+        else:
+            left_motor.run_timed(speed_sp=-speed, time_sp=c*(pseudo_distance-mov))
+            right_motor.run_timed(speed_sp=-speed, time_sp=c*(pseudo_distance-mov))
+            mov = pseudo_distance
+        sleep(c*epsilon/1000)
+    bumped_right=touch_sensor_right.is_pressed
+    bumped_left=touch_sensor_left.is_pressed
+    return mov, bumped_left, bumped_right
 
-def move(left_motor, right_motor, touch_sensor_left, touch_sensor_right, uv_sensor, distance=10,speed=500, uvDist = 30):
+
+
+def moveWithUV(left_motor, right_motor, touch_sensor_left, touch_sensor_right, uv_sensor, uvDist = 30, distance=10,speed=500):
     c = 50
     mov = 0
     bumped_right=False
