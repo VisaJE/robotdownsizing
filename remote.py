a77d9bf7f9a84c73b023609291acd2f0437e38e8
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-10-20 19:12:52 +0300

    dd

--- remote.py
+++ remote.py
@@ -12,32 +12,23 @@ class Remote:
         self.mm = Motor('outC')
     def inputLoop(self):
         print("remote start")
-        screen = curses.initscr()
-        curses.cbreak()
-        screen.keypad(1)
-
-        screen.addstr("Enter q to quit ")
-        screen.refresh()
         inp = ''
 
-        while inp != ord('q'):
-            inp = screen.getch()
-            screen.addch(inp)
-            screen.refresh()
+        while inp != 'q':
+            inp = input()
             if inp == 'a':
-                self.rm.run_timed(time_sp=250, speed_sp=-400, stop_action='brake')
+                self.rm.run_timed(time_sp=250, speed_sp=-450, stop_action='brake')
             elif inp == 's':
-                self.lm.run_timed(time_sp=250, speed_sp=400, stop_action='brake')
-                self.rm.run_timed(time_sp=250, speed_sp=400, stop_action='brake')
+                self.lm.run_timed(time_sp=250, speed_sp=450, stop_action='brake')
+                self.rm.run_timed(time_sp=250, speed_sp=450, stop_action='brake')
             elif inp == 'd':
-                self.lm.run_timed(time_sp=250, speed_sp=-400, stop_action='brake')
+                self.lm.run_timed(time_sp=250, speed_sp=-450, stop_action='brake')
             elif inp == 'w':
-                self.lm.run_timed(time_sp=250, speed_sp=-400, stop_action='brake')
-                self.rm.run_timed(time_sp=250, speed_sp=-400, stop_action='brake')
+                self.lm.run_timed(time_sp=250, speed_sp=-450, stop_action='brake')
+                self.rm.run_timed(time_sp=250, speed_sp=-450, stop_action='brake')
             elif inp == 'r':
-                self.mm.run_timed(time_sp=250, speed_sp=-400, stop_action='brake')
+                self.mm.run_timed(time_sp=250, speed_sp=-450, stop_action='brake')
             elif inp == 't':
-                self.mm.run_timed(time_sp=250, speed_sp=400, stop_action='brake')
+                self.mm.run_timed(time_sp=250, speed_sp=450, stop_action='brake')
             sleep(0.25)
-        curses.endwin()
         print("remote exit")
