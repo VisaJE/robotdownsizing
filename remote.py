2a80aa1e997e42f56273016455bdcb4354608162
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-10-20 18:23:35 +0300

    dd

---
+++ remote.py
@@ -0,0 +1,25 @@
+from ev3dev.ev3 import *
+from time   import sleep
+from collections import namedtuple
+from functools import reduce
+from math import *
+
+class Remote:
+    def __init__(self):
+        lm = LargeMotor('outB')
+		rm = LargeMotor('outA')
+	def inputLoop(self):
+		while True:
+			inp = input("")
+			if inp == 'q':
+				break
+			if inp == 'a':
+				lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+			elif inp == 's':
+				lm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
+				rm.run_timed(time_sp=250, speed_sp=300, stop_action='brake')
+			elif inp == 'd':
+				rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+			elif inp == 'w':
+				lm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
+				rm.run_timed(time_sp=250, speed_sp=-300, stop_action='brake')
