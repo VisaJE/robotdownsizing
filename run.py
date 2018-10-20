75ee922d7fbc0fd988b7bd93fb0bb4f0d9b46f82
coldDitch <juuso.kylmaoja@aalto.fi>
2018-10-20 15:21:50 +0300

    asda

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from ev3dev.ev3 import *
 from pylvas import *
 lm = LargeMotor('outB')
 rm = LargeMotor('outA')
-touch_sensor_right=TouchSensor('In2')
-touch_sensor_left=TouchSensor('In3')
+touch_sensor_right=TouchSensor('in2')
+touch_sensor_left=TouchSensor('in3')
 move(lm, rm, touch_sensor_left, touch_sensor_right, distance=10)
 #solver=new Pylvas_solver(lm,rm,touch_sensor_right,touch_sensor_left)
 #solver.execute()
