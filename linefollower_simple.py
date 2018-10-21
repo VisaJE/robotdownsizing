1bebc79df030f0c9845b3542cf6ee48c1f36121c
Arttu Tolvanen <arttu.tolvanen@aalto.fi>
2018-10-21 12:07:09 +0300

    vituixmän

--- linefollower_simple.py
+++ linefollower_simple.py
@@ -6,36 +6,25 @@ from turn_robot import *
 
 class Linefollower:
     
-    def __init__(self, cs):
-        self.cl=cs.cl
-        self.left_motor=LargeMotor('outB')
-        self.right_motor=LargeMotor('outA')
-        self.touch_sensor_left=TouchSensor('in3')
-        self.touch_sensor_right=TouchSensor('in2')
-        self.cs = cs
-        self.line_following_on = True
-
-
-        self.speed = int(input("SPEED: "))
-        self.run_time = int(input("RUNTIME: "))
-        self.buffer_size = int(input("BUFFERSIZE: "))
+    def __init__(self, cl, left_motor, right_motor, touch_sensor_left, touch_sensor_right):
         self.on_dark=True
         self.on_light=False
-        self.mode='COL-REFLECT'
+        cl.mode='COL-REFLECT'
         self.last_darks = []
       
         self.side_of_line = False # False if right, True if left
         self.speed = 200
         self.run_time = 50
         self.buffer_size = 4
+        self.line_following_on = False
     
     def follow_line(self):
     
         if (self.line_following_on):
         
-            self.on_light=self.cl.value()>=25
-            self.on_dark=self.cl.value()<25
-            print(self.cl.value())
+            self.on_light=cl.value()>=25
+            self.on_dark=cl.value()<25
+            print(cl.value())
     
             if (self.on_dark):
                 self.last_darks.append(True)
@@ -49,8 +38,8 @@ class Linefollower:
             moveleft = (self.on_light and self.side_of_line) or (self.on_dark and not self.side_of_line)
     
             if (turnright):
-                move(self.left_motor, self.right_motor, self.touch_sensor_left, self.touch_sensor_right, distance=7)
-                turn_right(self.left_motor, self.right_motor,degrees=60)
+                move(left_motor, right_motor, touch_sensor_left, touch_sensor_right, distance=7)
+                turn_right(left_motor, right_motor,degrees=60)
                 #turn right
                 #left_motor.run_timed(time_sp=, speed_sp = -self.speed, stop_action='brake')
                 #right_motor.run_timed(time_sp=, speed_sp = -self.speed, stop_action='brake')
@@ -58,34 +47,58 @@ class Linefollower:
                 #right_motor.run_timed(time_sp=self.run_time, speed_sp=self.speed, stop_action='brake')
                 
             elif (turnleft):
-                self.right_motor.run_timed(time_sp=self.run_time, speed_sp=-self.speed, stop_action='brake')
-                self.left_motor.run_timed(time_sp=self.run_time, speed_sp=self.speed, stop_action='brake')
+                right_motor.run_timed(time_sp=self.run_time, speed_sp=-self.speed, stop_action='brake')
+                left_motor.run_timed(time_sp=self.run_time, speed_sp=self.speed, stop_action='brake')
             
             elif(moveright):
+<<<<<<< HEAD
                  self.left_motor.run_timed(time_sp=self.run_time, speed_sp=-self.speed, stop_action='brake')
                  #self.right_motor.run_timed(time_sp=self.run_time, speed_sp=-0.3*self.speed, stop_action='brake')
+=======
+                 left_motor.run_timed(time_sp=self.run_time, speed_sp=-self.speed, stop_action='brake')
+                 right_motor.run_timed(time_sp=self.run_time, speed_sp=-0.3*self.speed, stop_action='brake')
+>>>>>>> vituixmän
             
             elif(moveleft): 
-                self.right_motor.run_timed(time_sp=self.run_time, speed_sp=-self.speed, stop_action='brake')
+                right_motor.run_timed(time_sp=self.run_time, speed_sp=-self.speed, stop_action='brake')
                 
+<<<<<<< HEAD
                 #self.left_motor.run_timed(time_sp=self.run_time, speed_sp=-0.3*self.speed, stop_action='brake')
+=======
+                left_motor.run_timed(time_sp=self.run_time, speed_sp=-0.3*self.speed, stop_action='brake')
+>>>>>>> vituixmän
                 #print("liiku oikealle")
     
-            if (self.touch_sensor_left.is_pressed or self.touch_sensor_right.is_pressed):
+            if (touch_sensor_left.is_pressed or touch_sensor_right.is_pressed):
                 print("COLLISION! MOVING BACKWARDS")
                 #Sound.speak('Perkele',espeak_opts='-a 200 -v finnish').wait()
-                move_backwards(self.left_motor, self.right_motor,distance=3)
+                move_backwards(left_motor, right_motor,distance=4)
     
             if (len(self.last_darks) > self.buffer_size):
                 self.last_darks.pop(0)
             #print("kierros")
-            sleep(self.run_time/1000)     
+            sleep(self.run_time/1000)            
 
+<<<<<<< HEAD
     def lineLoop(self):
         while(True):
             self.follow_line()
+=======
+>>>>>>> vituixmän
+
+cl=ColorSensor()
+left_motor=LargeMotor('outB')
+right_motor=LargeMotor('outA')
+touch_sensor_left=TouchSensor('in3')
+touch_sensor_right=TouchSensor('in2')
+
+lf = Linefollower(cl, left_motor, right_motor,touch_sensor_left, touch_sensor_right)
+lf.line_following_on = True
 
 
+lf.speed = int(input("SPEED: "))
+lf.run_time = int(input("RUNTIME: "))
+lf.buffer_size = int(input("BUFFERSIZE: "))
 
-
-
+while(True):
+    lf.follow_line()
